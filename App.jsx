6c70659b95c9694d5613ccdec9a2a558427891bd
Alain Perez <alain.perez@openbravo.com>
2024-12-11 11:21:22 -0500

    Added  paper

---
+++ App.jsx
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {PaperProvider} from 'react-native-paper';
+
+function App(){
+  return (
+    <PaperProvider>
+      <View>
+        <Text>1233</Text>
+      </View>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
